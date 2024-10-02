c691f9f5a8069c8c1248d57fda40651e13135f5f
zoe-lry <zoeliruiyi@gmail.com>
2024-10-01 21:25:49 -0700

    Lab 4. adding 3 failure tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,7 +9,34 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("fixme", items[0].name)
+
+
+    def test_vest_item_greater_than_50(self):
+        vest = "test"
+        items = [Item(vest, 1, 60)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+        self.assertEqual(items[0].quality, None)
+
+
+    def test_vest_item_with_wrong_quality_type(self):
+        vest = "test"
+        items = [Item(vest, "aa", "aa")]
+        # gr = GildedRose(items)
+
+        self.assertEqual(items[0].quality, None)
+    
+
+    def test_Conjured_degrade_twice_as_normal(self):
+        vest = "Conjured"
+        items = [Item(vest, 10, 10)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        self.assertEqual(items[0].quality, 8)
 
 
 if __name__ == '__main__':
